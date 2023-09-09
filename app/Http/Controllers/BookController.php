bad5b454c9cbb144d02efbfc2c92e84e520415d0
Dev-Ismael <dev.ismael305@gmail.com>
2023-09-09 21:52:55 +0300

    Update App Service Provider

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,11 +2,72 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Book;
+use App\Repositories\RepositoryInterface;
+use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
+    private $bookRepository;
 
+    public function __construct(RepositoryInterface $bookRepository)
+    {
+        $this->bookRepository = $bookRepository;
+    }
 
+    public function index()
+    {
+        $books = $this->bookRepository->getAll();
+        return response()->json(['data' => $books]);
+    }
 
+    public function create()
+    {
+        // You can return a JSON response for create if needed.
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'published_year' => 'required|integer',
+        ]);
+
+        $createdBook = $this->bookRepository->create($data);
+
+        return response()->json(['data' => $createdBook], 201); // 201 Created status code
+    }
+
+    public function show($id)
+    {
+        $book = $this->bookRepository->find($id);
+        return response()->json(['data' => $book]);
+    }
+
+    public function edit($id)
+    {
+        // You can return a JSON response for edit if needed.
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+            'title' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'published_year' => 'required|integer',
+        ]);
+
+        $updatedBook = $this->bookRepository->update($id, $data);
+
+        return response()->json(['data' => $updatedBook]);
+    }
+
+    public function destroy($id)
+    {
+        $this->bookRepository->delete($id);
+
+        return response()->json(['message' => 'Book deleted successfully']);
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\BookRepository;
+use App\Repositories\RepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -9,9 +11,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        $this->app->bind(RepositoryInterface::class, BookRepository::class);
     }
 
     /**
