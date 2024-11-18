3a10b568d32428839dbe9d69122ff2470ba638ec
unknown <Abdelrahman Ismail>
2024-11-18 12:39:02 +0200

    add task scheduling at user status

---
+++ app/Console/Commands/UpdateUserStatus.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+class UpdateUserStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'UpdateUserStatus';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $users = User::get();
+        foreach ($users as $user) {
+            $user->status = 1;
+            $user->save();
+        }
+    }
+}
